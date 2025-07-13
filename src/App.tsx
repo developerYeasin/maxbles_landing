b7a833175af6d0641b447adc6c9a8bb4e6878687
[dyad] <yea13sin@gmail.com>
2025-07-14 00:22:23 +0600

    [dyad] Fixed a syntax error in the App.tsx import statement. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } => "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -11,7 +11,6 @@ import PortfolioPage from "./pages/PortfolioPage";
 import TestimonialsPage from "./pages/TestimonialsPage";
 import BlogPage from "./pages/BlogPage";
 import ContactPage from "./pages/ContactPage";
-// import CustomCursor from "@/components/CustomCursor"; // Removed CustomCursor import
 
 const queryClient = new QueryClient();
 
@@ -20,7 +19,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* CustomCursor removed */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
