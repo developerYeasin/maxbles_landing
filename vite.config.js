8140f297797647642e1042bd104e8c928ef713e2
[dyad] <yea13sin@gmail.com>
2025-07-14 15:17:42 +0600

    [dyad] Updated `vite.config.js` to use a dynamic import for `dyadComponentTagger` to resolve a module loading error. - wrote 1 file(s)

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from "vite";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [dyadComponentTagger(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(async () => {
+  const dyadComponentTagger = (await import("@dyad-sh/react-vite-component-tagger")).default;
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [dyadComponentTagger(), react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
