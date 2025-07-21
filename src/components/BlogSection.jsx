fb735b8a3509d00b8465e06482cb41f310f93ce6
[dyad] <yea13sin@gmail.com>
2025-07-21 22:59:36 +0600

    [dyad] Fixed a syntax error in the BlogSection component. - wrote 1 file(s)

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } => 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils.js";
