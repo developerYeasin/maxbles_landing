c573ee47369c3cf23aa482f95eae0789a740da50
[dyad] <yea13sin@gmail.com>
2025-08-06 19:59:14 +0600

    [dyad] Switched to using favicon.ico from the public folder - wrote 2 file(s)

--- src/components/PageMeta.jsx
+++ src/components/PageMeta.jsx
@@ -5,7 +5,6 @@ const PageMeta = ({ title, description }) => {
   const siteName = 'MaxBles Digital';
   const defaultTitle = `${siteName} - DIGITAL CONSULTING AGENCY`;
   const defaultDescription = 'MaxBles Digital is a leading digital consulting agency offering expert services in web development, UI/UX design, digital marketing, and automation. Explore our portfolio and read our blog for the latest insights.';
-  const logoUrl = "https://res.cloudinary.com/dbk7ixyvd/image/upload/v1754242560/maxbles-final-color-1-1536x354_xadpkb.png";
 
   // If a title is provided, format it as "MaxBles Digital - {title}". Otherwise, use the default homepage title.
   const pageTitle = title ? `${siteName} - ${title}` : defaultTitle;
@@ -14,9 +13,6 @@ const PageMeta = ({ title, description }) => {
     <Helmet>
       <title>{pageTitle}</title>
       <meta name="description" content={description || defaultDescription} />
-      <link rel="icon" href={logoUrl} type="image/png" />
-      <link rel="shortcut icon" href={logoUrl} type="image/png" />
-      <link rel="apple-touch-icon" href={logoUrl} />
     </Helmet>
   );
 };
